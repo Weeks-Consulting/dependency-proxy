fd2157dc4980bb18b5f85eed23e96d085a8b0066
Shawn Weeks <sweeks@weeksconsulting.us>
2026-04-09 18:03:55 +0000

    Initial Commit

---
+++ src/test/java/us/weeksconsulting/dependency_proxy/DependencyProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package us.weeksconsulting.dependency_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/us/weeksconsulting/dependency_proxy/TestDependencyProxyApplication.java
@@ -0,0 +1,11 @@
+package us.weeksconsulting.dependency_proxy;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestDependencyProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(DependencyProxyApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
