f5a6739bd9d6204794e4ef7d834d8680bf696387
Shawn Weeks <sweeks@weeksconsulting.us>
2026-04-15 20:32:27 +0000

    Made path dynamic

--- src/main/java/us/weeksconsulting/dependency_proxy/controllers/ProxyRoutingController.java
+++ src/main/java/us/weeksconsulting/dependency_proxy/controllers/ProxyRoutingController.java
@@ -13,6 +13,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestClient;
@@ -22,12 +23,18 @@ import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBo
 public class ProxyRoutingController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ProxyRoutingController.class);
 
-    @GetMapping("/{repository}/**")
-    public ResponseEntity<StreamingResponseBody> getRequest(@PathVariable String repository) {
-        String url = "https://archive.apache.org/dist/nifi/2.9.0/minifi-toolkit-2.9.0-bin.zip";
+    @GetMapping("*/{repository}/{*path}")
+    public ResponseEntity<StreamingResponseBody> getRequest(@PathVariable String repository, @PathVariable String path) {
+        LOGGER.info("repository: {}", repository);
+        LOGGER.info("path: {}", path);
+        
+        String baseUrl = "https://archive.apache.org";
 
         LOGGER.info("Returning RestClient");
-        return RestClient.create().get().uri(url).exchange((request, response) -> {
+        return RestClient.create().get().uri(baseUrl + path).exchange((request, response) -> {
+            LOGGER.info("Request URI: {}", request.getURI());
+            LOGGER.info("Request Headers: {}", request.getHeaders());            
+
             HttpHeaders responseHeaders = new HttpHeaders();
             responseHeaders.addAll(CONTENT_TYPE, response.getHeaders().get(CONTENT_TYPE));
 
