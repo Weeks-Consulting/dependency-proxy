3bf11486d691bd69fb1a57285070f037c3ea0f22
Shawn Weeks <sweeks@weeksconsulting.us>
2026-04-21 16:37:41 +0000

    Cleaning up configuration

---
+++ src/main/java/us/weeksconsulting/dependencyproxy/config/SpringConfiguration.java
@@ -0,0 +1,36 @@
+package us.weeksconsulting.dependencyproxy.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@ConfigurationPropertiesScan("us.weeksconsulting.dependencyproxy.config")
+@EnableAsync
+public class SpringConfiguration {
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("GithubLookup-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(connector -> connector.setAsyncTimeout(600000)); // Set async timeout to 10
+                                                                                         // minutes
+        return factory;
+    }
+
+}
